51d8943f6362f8becb5ac75cd3f9d1cbe53a857c
SalahEddine Hanaoui <hanaoui568@gmail.com>
2022-09-20 00:12:20 +0100

    ajout de la fonction acheter un produit +etat stock + ajouter au stock

---
+++ SAS YOUCODE/Final Product/main.c
@@ -0,0 +1,309 @@
+#include <stdio.h> 
+#include<stdlib.h>
+#include <string.h>
+struct product {
+	float price;
+    char code[20];
+    char name[20];
+    int quantity;
+
+};
+
+struct product products[100];
+
+int chosenItem, numberOfProducts = 0;
+
+int compare();
+void showMenu();
+void addAProduct();
+void showPage(int chosenItem);
+void chooseAnItem();
+void addMultipleProducts();
+void ShowProducts();
+void FindProductByCode();
+void FindProductByQuantity();
+void BuyAProduct();
+void AddToStock();
+void StateOfStock();
+
+
+int main()
+{
+    showMenu();
+    chooseAnItem();
+}
+// AFFICHAGE DE MENU
+void showMenu(){
+    printf("\n\t---  MENU  ---\n");
+    printf("\n> [1] Ajouter un nouveau produit");
+    printf("\n> [2] Ajouter plusieurs produits");
+    printf("\n> [3] Afficher tout les produits");
+    printf("\n> [4] Acheter un produit");
+    printf("\n> [5] Chercher un produit");
+    printf("\n> [6] Etat du stock");
+    printf("\n> [7] Ajouter au stock");
+    printf("\n> [8] Supprimer un produit");
+    printf("\n> [9] Afficher les statistiques");
+}
+// FONCTION D AJOUT D UN SEUL PRODUIT
+void addAProduct(){
+    printf("Ajouter un produit :\n\n");
+    
+    printf("Entrer le code du produit :\n=> ");
+    scanf("%s", products[numberOfProducts].code);
+    
+    printf("Entrer le nom du produit :\n=> ");
+    scanf("%s", products[numberOfProducts].name);
+    
+    printf("Entrer la quantite du produit:\n=> ");
+    scanf("%d", &products[numberOfProducts].quantity);
+    
+    printf("Entrer le prix du produit (HT):\n=> ");
+    scanf("%f", &products[numberOfProducts].price);
+    
+    numberOfProducts++;
+    
+    system("cls");
+    printf("Produit ajouter avec succees !! \n\n");
+    
+    showMenu();
+    chooseAnItem();
+    
+}
+// FONCTION DE SWITCH POUR ACCEDER AU CHOIX SUR LE MENU
+void showPage(int chosenItem) {
+    
+    switch(chosenItem){
+        case 1 :
+            system("cls");
+            addAProduct();
+            break;
+        case 2 :
+            system("cls");
+            addMultipleProducts();
+            break;
+        case 3 :
+            system("cls");
+            ShowProducts();
+            break;
+        case 4 :
+            system("cls");
+            BuyAProduct();
+            break;
+        case 5 :
+            system("cls");
+            int num;
+            printf("\n> [1] Chercher avec Code");
+            printf("\n> [2] Chercher avec Quantite");
+            scanf("%d",&num);
+            switch (num){
+            	case 1:
+            		system("cls");
+            		FindProductByCode();
+            		break;
+            	case 2:
+            		system("cls");
+					FindProductByQuantity();
+					break;	
+			}
+            break;
+        case 6 :
+            system("cls");
+            StateOfStock();
+            break;
+        case 7 :
+            system("cls");
+            AddToStock();
+            break;
+        case 8 :
+            system("cls");
+            printf("Supprimer produit");
+            break;
+        case 9 :
+            system("cls");
+            printf("Afficher les statistiques");
+            break;
+        default :
+            system("cls");
+            printf("Merci de choisir du MENU! \n");
+            showMenu();
+            break;
+    }
+}
+//FONCTION DE CHOIX SUR LE MENU QUI AJOUTE UN PRINTF ET SCANF POUR SELECTIONNER LE NUMERO
+void chooseAnItem(){
+    do {
+        printf("\n=> ");
+        scanf("%d", &chosenItem);
+        
+        showPage(chosenItem);
+        
+    } while(chosenItem < 1 || chosenItem > 9);
+
+}
+
+//FONCTION D AJOUT DE PLUSIEUR PRODUIT
+void addMultipleProducts(){
+	int i;
+	int n;
+	printf("\n>Entrer combien de produit vous souhaitez ajouter :");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+
+	printf("Ajouter un produit :\n\n");
+    
+    printf("Entrer le code du produit :\n=> ");
+    scanf("%s", products[numberOfProducts].code);
+    
+    printf("Entrer le nom du produit :\n=> ");
+    scanf("%s", products[numberOfProducts].name);
+    
+    printf("Entrer la quantite du produit:\n=> ");
+    scanf("%d", &products[numberOfProducts].quantity);
+    
+    printf("Entrer le prix du produit (HT):\n=> ");
+    scanf("%f", &products[numberOfProducts].price);
+    
+    numberOfProducts++;
+    
+    system("cls");
+    printf("Produit ajouter avec succees !! \n\n");
+		
+	}
+	sleep(1);
+	system("cls");
+	showMenu();
+	chooseAnItem();
+}
+
+//AFFICHER TOUT LES PRODUITS
+void ShowProducts(){
+	//if : pour si il n y a pas de produit ajouter sur le systeme sa retourne au menu automatiquement apres 4 secondes
+	if(numberOfProducts==0){
+		printf("\n>Il n y a pas de produit dans le systeme...Retour au menu");	
+		sleep(4);
+		system("cls");
+		showMenu();
+		chooseAnItem();
+	}
+	//lister tout les produits dans l ordre décroissant
+	qsort(products, numberOfProducts, sizeof(struct product), compare);
+	//for :pour afficher les informations du produit qui sont ajouter sur le systeme
+	int i;
+	for(i=0;i<numberOfProducts;i++){
+	printf("\n->Nom du produit :%s\t ->Prix du produit en HT :%.2f\t ->Prix du produit en TTC :%.2f\n",products[i].name,products[i].price,products[i].price +((products[i].price)*0.15));
+	}
+	system("Pause");
+	system("cls");
+	showMenu();
+	chooseAnItem();
+	// CALCULE DU PRIX DU HT EN TTC 15% : products[i].price +((products[i].price)*0.15)
+}
+
+// Fonction de comparaison POUR LE TRIAGE DECROISSANT
+int compare(const void *x_void, const void *y_void)
+{
+ 
+  int x = *(int *)x_void;
+  int y = *(int *)y_void;
+
+  return y - x;
+}
+
+//rechercher un produit avec son code
+void FindProductByCode(){
+	char ProductCode[20];
+	int i;
+	
+	printf("Entrer le Code du produit : ");
+	scanf("%s",ProductCode);
+	
+	for(i=0;i<numberOfProducts;i++){
+		if (strcmp(products[i].code,ProductCode) == 0 )
+		printf("\n->Code du produit : %s\t ->Nom du produit :%s\t ->Quantite du produit : %d\t ->Prix du produit en HT :%.2f\t ->Prix du produit en TTC :%.2f\n", products[i].code,products[i].name,products[i].quantity,products[i].price,products[i].price +((products[i].price)*0.15));
+	}
+	system("Pause");
+	system("cls");
+	showMenu();
+	chooseAnItem();
+}
+//recher un produit avec la quantité
+void FindProductByQuantity(){
+	int ProductQuantity;
+	int i;
+	
+	printf("Entrer la Quantite du produit : ");
+	scanf("%d",&ProductQuantity);
+	
+	for(i=0;i<numberOfProducts;i++){
+		if (products[i].quantity==ProductQuantity )
+		printf("\n->Quantite du produit : %d \t ->Code du produit : %s \n->Nom du produit :%s\t ->Prix du produit en HT :%.2f\t->Prix du produit en TTC :%.2f\n", products[i].quantity,products[i].code,products[i].name,products[i].price,products[i].price +((products[i].price)*0.15));
+	}
+	system("Pause");
+	system("cls");
+	showMenu();
+	chooseAnItem();
+}
+//fontion acheter un produit / deduire du stock
+void BuyAProduct(){
+	//find product by code
+	char ProductCode[20];
+	int i;
+	int number;
+	int x;
+	printf("Entrer le Code du produit : ");
+	scanf("%s",ProductCode);
+
+	for(i=0;i<numberOfProducts;i++){
+		if (strcmp(products[i].code,ProductCode) == 0 ){
+		x=i;// ajouter pour stocker l indice du produit
+		printf("\n->Code du produit : %s\t ->Nom du produit :%s\t ->Quantite du produit : %d\t ->Prix du produit en HT :%.2f\t ->Prix du produit en TTC :%.2f\n", products[i].code,products[i].name,products[i].quantity,products[i].price,products[i].price +((products[i].price)*0.15));
+		}
+	}
+	printf("\n->Entrer la valeur a deduir du stock :\n");
+	scanf("%d",&number);
+	printf("\n->La valeur dans le stock est maintenant: %d\n",products[x].quantity-=number);
+	system("Pause");
+	system("cls");
+	showMenu();
+	chooseAnItem();
+}
+//fonction ajouter au stock
+void AddToStock(){
+	//find product by code
+	char ProductCode[20];
+	int i;
+	int number;
+	int x;
+	printf("Entrer le Code du produit : ");
+	scanf("%s",ProductCode);
+
+	for(i=0;i<numberOfProducts;i++){
+		if (strcmp(products[i].code,ProductCode) == 0 ){
+		x=i;// ajouter pour stocker l indice du produit
+		printf("\n->Code du produit : %s\t ->Nom du produit :%s\t ->Quantite du produit : %d\t ->Prix du produit en HT :%.2f\t ->Prix du produit en TTC :%.2f\n", products[i].code,products[i].name,products[i].quantity,products[i].price,products[i].price +((products[i].price)*0.15));
+		}
+	}
+	printf("\n->Entrer la valeur a ajouter au stock :\n");
+	scanf("%d",&number);
+	printf("\n->La valeur dans le stock est maintenant: %d\n",products[x].quantity+=number);
+	system("Pause");
+	system("cls");
+	showMenu();
+	chooseAnItem();
+}
+
+//fonction pour afficher l etat du stock les produits<3
+void StateOfStock(){
+	int i;
+	
+	for(i=0;i<numberOfProducts;i++){
+		if (products[i].quantity<3){
+			printf("\n->Code du produit : %s\t ->Nom du produit :%s\t ->Quantite du produit : %d\t ->Prix du produit en HT :%.2f\t ->Prix du produit en TTC :%.2f\n", products[i].code,products[i].name,products[i].quantity,products[i].price,products[i].price +((products[i].price)*0.15));
+		}
+	}
+	system("Pause");
+	system("cls");
+	showMenu();
+	chooseAnItem();
+}
